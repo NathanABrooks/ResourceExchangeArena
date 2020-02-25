41a183a28c6fe20d41f952787388ffd324b278c2
Nathan Brooks <nathan.a.brooks1997@gmail.com>
2020-02-25 12:36:07 +0000

    Run where agents always copy higher satisfaction

--- src/resourceexchangearena/SocialLearning.java
+++ src/resourceexchangearena/SocialLearning.java
@@ -41,11 +41,12 @@ class SocialLearning {
             double agentSatisfaction = agent.calculateSatisfaction(null);
             double observedSatisfaction = (double) agentToCopy[1] / 4;
             if (agentSatisfaction < observedSatisfaction) {
-                double difference = observedSatisfaction - agentSatisfaction;
-                double threshold = ResourceExchangeArena.random.nextDouble();
-                if (difference > threshold) {
-                    agent.setType(agentToCopy[2]);
-                }
+//                double difference = observedSatisfaction - agentSatisfaction;
+//                double threshold = ResourceExchangeArena.random.nextDouble();
+//                if (difference > threshold) {
+//                    agent.setType(agentToCopy[2]);
+//                }
+                agent.setType(agentToCopy[2]);
             }
             unselectedAgents.remove(Integer.valueOf(agent.agentID));
         }
