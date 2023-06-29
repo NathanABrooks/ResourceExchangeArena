dab383bc58fcfae1fc412466de22894d0e434f55
refracc <40345422@live.napier.ac.uk>
2023-06-29 16:27:33 +0100

    Changed Inflect

--- src/resource_exchange_arena/Inflect.java
+++ src/resource_exchange_arena/Inflect.java
@@ -2,26 +2,20 @@ package resource_exchange_arena;
 
 class Inflect {
     /**
-     * Takes an agentType and converts it from it's integer format to a descriptive string name to organise the data
-     * output by the simulation.
+     * Takes an agentType and converts it from its {@link Integer} format to a descriptive {@link String} name to organise the data output by the simulation.
      *
-     * @param agentType Integer value representing the type of the agent as a potentially unclear number.
-     * @return String Returns the given agentType as a descriptive string.
+     * @param agentType {@link Integer} value representing the type of the {@link Agent} as a potentially unclear number.
+     * @return The given agentType as a descriptive {@link String}.
      */
     static String getHumanReadableAgentType(int agentType) {
-        String name;
+        String name = switch (agentType) {
+            case ResourceExchangeArena.SOCIAL -> "Social";
+            case ResourceExchangeArena.SELFISH -> "Selfish";
+            default ->
+                // If a human-readable name doesn't exist, return the {@link Integer} agentType as a string.
+                    String.valueOf(agentType);
+        };
         // Names match types specified by constant integers for the ExchangeArena.
-        switch(agentType) {
-            case ResourceExchangeArena.SOCIAL:
-                name = "Social";
-                break;
-            case ResourceExchangeArena.SELFISH:
-                name = "Selfish";
-                break;
-            default:
-                // If a human readable name doesnt exist, return the integer agentType as a string.
-                name = String.valueOf(agentType);
-        }
         return name;
     }
 }
